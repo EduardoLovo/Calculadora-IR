cb3438cf5e34cdda015f81c82520bb7da7b8ab80
EduardoLovo <eduardo.llovo@gmail.com>
2022-03-16 13:59:39 -0300

    Create Form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 function App() {
 
+  // Exemplos ========
+
   const [notas, setNotas] = useState([
     {
       id: 1,
@@ -22,12 +24,134 @@ function App() {
     }
   ])
 
+  // =========================================
+
+  // Formulario =======
+
+  const [data, setData] = useState("");
+  const [tipo, setTipo] = useState("");
+  const [preco, setPreco] = useState("");
+  const [quantidade, setQuantidade] = useState("");
+  const [taxa, setTaxa] = useState("");
+
+  const handleDataChange = (e) => {
+    setData(e.target.value);
+  }
+  const handleTipoChange = (e) => {
+    setTipo(e.target.value);
+  }
+  const handlePrecoChange = (e) => {
+    setPreco(e.target.value);
+  }
+  const handleQuantidadeChange = (e) => {
+    setQuantidade(e.target.value);
+  }
+  const handleTaxaChange = (e) => {
+    setTaxa(e.target.value);
+  }
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setNotas([
+      ...notas,
+      {
+        data: data,
+        tipo: tipo,
+        preco: preco,
+        quantidade: quantidade,
+        taxa: taxa
+      }
+    ])
+
+    setData('')
+    setTipo('')
+    setPreco('')
+    setQuantidade('')
+    setTaxa('')
+
+  }
+
 
   // =========================================
+
+
 
   return (
 
     <div className="App">
+
+      {/* Formulario ========== */}
+
+      <div className='divAdd'>
+        <form className='add' onSubmit={handleSubmit}>
+          <div className='input'>
+            <label>Data</label>
+            <input
+              type='date'
+
+              value={data}
+              placeholder='um'
+              onChange={handleDataChange}
+            />
+          </div>
+
+          <div className='input'>
+            <label >Tipo</label>
+            <select name="select"
+              onChange={handleTipoChange}
+              value={tipo}>
+              <option placeholder='...'></option>
+              <option value="Compra">Compra</option>
+              <option value="Venda" selected>Venda</option>
+            </select>
+          </div>
+
+          <div className='input'>
+            <label >Preço</label>
+            <input
+              type='number'
+              step="0.01"
+              min="0"
+              placeholder='R$ 59.90'
+              value={preco}
+              onChange={handlePrecoChange}
+            />
+          </div>
+
+          <div className='input'>
+            <label >Quantidade</label>
+            <input
+              type='number'
+              placeholder='Ex. 5'
+              value={quantidade}
+              onChange={handleQuantidadeChange}
+            />
+          </div>
+
+          <div className='input'>
+            <label >Taxa</label>
+            <input
+              type='number'
+              step="0.01"
+              min="0"
+              placeholder='Ex. 8.50'
+              value={taxa}
+              onChange={handleTaxaChange}
+            />
+          </div>
+          <div className='divBtn'>
+            <button type="submit" value="Add" className='btnAdd'>Add</button>
+
+          </div>
+        </form>
+      </div>
+
+      {/* ====================================== */}
+
+
+      {/* Lista de Notas ========== */}
 
       <div className='notasView'>
 
@@ -51,7 +175,7 @@ function App() {
 
                 <p>{f.tipo}</p>
 
-                <p>{f.preco}</p>
+                <p>R$ {f.preco}</p>
 
                 <p>{f.quantidade}</p>
 
@@ -66,6 +190,11 @@ function App() {
         </ul>
 
       </div>
+
+      {/* ====================================== */}
+
+
+
     </div>
   );
 }
